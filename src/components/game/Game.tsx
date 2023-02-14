3447aaeebc2ec699809885fbe0b404f7503afff2
fhgaha <antsy1992@gmail.com>
2023-02-14 04:29:02 +0300

    field decorations

--- src/components/game/Game.tsx
+++ src/components/game/Game.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useKeyPress } from '../../hooks/useKeyPress'
 import st from './game.module.css'
 
@@ -6,10 +6,15 @@ const Game = () => {
   const width = 10
   const height = 20
 
-  const [field, setField] = useState(
-    new Array(height).fill(0)
-      .map(() => new Array(width).fill(0))
+  const [field, setField] = useState<number[][]>(
+    // new Array(height).fill(0).map(() => new Array(width).fill(0))
+    Array.from({ length: height }, () => Array<number>(width).fill(0))
   )
+
+  useEffect(() => {
+    updateField(2, 3, 1, setField,)
+  }, [])
+
   const up: boolean = useKeyPress('ArrowUp')
   const down: boolean = useKeyPress('ArrowDown')
   const left: boolean = useKeyPress('ArrowLeft')
@@ -30,14 +35,29 @@ const Game = () => {
 
 export default Game
 
-function display2DArrayAsTable(field: any[][]): React.ReactNode {
-  return field.map((rows, i) => {
-    return <ul className={st['ul-field']} key={'ul-' + i}>
-      {rows.map((cols, j) => {
-        return <li key={"li-" + j}>
-          {cols}
-        </li>
-      })}
-    </ul>
+function updateField(row: number, col: number, value: number,
+  setField: React.Dispatch<React.SetStateAction<number[][]>>) {
+  setField(prevField => {
+    const newF = [...prevField]
+    newF[row][col] = value
+    return newF
   })
+}
+
+function display2DArrayAsTable(field: number[][]): React.ReactNode {
+  let result = field.map((rows, i) => {
+    return (
+      <ul className={st['ul-field']} key={'ul-' + i}>
+        {rows.map((col, j) => {
+          return (
+            <li key={"li-" + j}>
+              {rows[j] == 0 ? '.' : '*'}
+            </li>)
+        })}
+      </ul>
+    )
+  })
+  console.log(result);
+  return result
+
 }
